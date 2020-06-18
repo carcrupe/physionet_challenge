b7020668327ea5bb1b9e10c8431017467bcb1520
carcrupe <56585307+carcrupe@users.noreply.github.com>
2020-06-18 11:00:28 +0200

    Add files via upload
    first model approach improved. Feature extraction of difference in time between R peaks

--- get_12ECG_features.py
+++ get_12ECG_features.py
@@ -104,6 +104,8 @@ def bandpass_filter(data, lowcut, highcut, signal_freq, filter_order):
         b, a = butter(filter_order, [low, high], btype="band")
         y = lfilter(b, a, data)
         return y
+    
+
 
 def findpeaks(data, spacing=1, limit=None):
         """
@@ -135,6 +137,7 @@ def findpeaks(data, spacing=1, limit=None):
         ind = ind.reshape(ind.size)
         if limit is not None:
             ind = ind[data[ind] > limit]
+        
         return ind
 
 
@@ -169,185 +172,40 @@ def get_12ECG_features(data, header_data):
     
 #   We are only using data from lead1
     peaks,idx = detect_peaks(data[0],sample_Fs,gain_lead[0])
+    
+    #Take the difference in time between R peaks
+    idx_diff = np.diff(idx)
    
-#   mean
-    mean_RR = np.mean(idx/sample_Fs*1000)
+    mean_RR = np.mean(idx_diff/sample_Fs*1000)
     mean_Peaks = np.mean(peaks*gain_lead[0])
 
 #   median
-    median_RR = np.median(idx/sample_Fs*1000)
+    median_RR = np.median(idx_diff/sample_Fs*1000)
     median_Peaks = np.median(peaks*gain_lead[0])
 
 #   standard deviation
-    std_RR = np.std(idx/sample_Fs*1000)
+    std_RR = np.std(idx_diff/sample_Fs*1000)
     std_Peaks = np.std(peaks*gain_lead[0])
 
-#   variance
-    var_RR = stats.tvar(idx/sample_Fs*1000)
-    var_Peaks = stats.tvar(peaks*gain_lead[0])
-
 #   Skewness
-    skew_RR = stats.skew(idx/sample_Fs*1000)
+    skew_RR = stats.skew(idx_diff/sample_Fs*1000)
     skew_Peaks = stats.skew(peaks*gain_lead[0])
 
 #   Kurtosis
-    kurt_RR = stats.kurtosis(idx/sample_Fs*1000)
+    kurt_RR = stats.kurtosis(idx_diff/sample_Fs*1000)
     kurt_Peaks = stats.kurtosis(peaks*gain_lead[0])
 
-    features = np.hstack([age,sex,mean_RR,mean_Peaks,median_RR,median_Peaks,std_RR,std_Peaks,var_RR,var_Peaks,skew_RR,skew_Peaks,kurt_RR,kurt_Peaks])
+    features = np.hstack([age,sex,mean_RR,mean_Peaks,median_RR,median_Peaks,std_RR,std_Peaks,skew_RR,skew_Peaks,kurt_RR,kurt_Peaks])
 
   
     return features
 
+def get_diagnosis(input_file):
+    for lines in input_file:
+        if lines.startswith('#Dx'):
+            tmp = lines.split(': ')[1].split(',')
+            for c in tmp:
+                tmp = c.strip()        
+    return tmp
 
-
-def ecg_wave_detector(ecg, rpeaks):
-    """
-    Returns the localization of the P, Q, T waves. This function needs massive help!
-    https://neurokit.readthedocs.io/en/latest/_modules/neurokit/bio/bio_ecg_preprocessing.html
-
-    Parameters
-    ----------
-    ecg : list or ndarray
-        ECG signal (preferably filtered).
-    rpeaks : list or ndarray
-        R peaks localization.
-
-    Returns
-    ----------
-    ecg_waves : dict
-        Contains wave peaks location indices.
-
-    Example
-    ----------
-    >>> import neurokit as nk
-    >>> ecg =  nk.ecg_simulate(duration=5, sampling_rate=1000)
-    >>> ecg = nk.ecg_preprocess(ecg=ecg, sampling_rate=1000)
-    >>> rpeaks = ecg["ECG"]["R_Peaks"]
-    >>> ecg = ecg["df"]["ECG_Filtered"]
-    >>> ecg_waves = nk.ecg_wave_detector(ecg=ecg, rpeaks=rpeaks)
-    >>> nk.plot_events_in_signal(ecg, [ecg_waves["P_Waves"], ecg_waves["Q_Waves_Onsets"], ecg_waves["Q_Waves"], list(rpeaks), ecg_waves["S_Waves"], ecg_waves["T_Waves_Onsets"], ecg_waves["T_Waves"], ecg_waves["T_Waves_Ends"]], color=["green", "yellow", "orange", "red", "black", "brown", "blue", "purple"])
-
-    Notes
-    ----------
-    *Details*
-
-    - **Cardiac Cycle**: A typical ECG showing a heartbeat consists of a P wave, a QRS complex and a T wave.The P wave represents the wave of depolarization that spreads from the SA-node throughout the atria. The QRS complex reflects the rapid depolarization of the right and left ventricles. Since the ventricles are the largest part of the heart, in terms of mass, the QRS complex usually has a much larger amplitude than the P-wave. The T wave represents the ventricular repolarization of the ventricles. On rare occasions, a U wave can be seen following the T wave. The U wave is believed to be related to the last remnants of ventricular repolarization.
-
-    *Authors*
-
-    - `Dominique Makowski <https://dominiquemakowski.github.io/>`_
-    """
-    q_waves = []
-    p_waves = []
-    q_waves_starts = []
-    s_waves = []
-    t_waves = []
-    t_waves_starts = []
-    t_waves_ends = []
-    for index, rpeak in enumerate(rpeaks[:-3]):
-
-        try:
-            epoch_before = np.array(ecg)[int(rpeaks[index-1]):int(rpeak)]
-            epoch_before = epoch_before[int(len(epoch_before)/2):len(epoch_before)]
-            epoch_before = list(reversed(epoch_before))
-
-            q_wave_index = np.min(find_peaks(epoch_before))
-            q_wave = rpeak - q_wave_index
-            p_wave_index = q_wave_index + np.argmax(epoch_before[q_wave_index:])
-            p_wave = rpeak - p_wave_index
-
-            inter_pq = epoch_before[q_wave_index:p_wave_index]
-            inter_pq_derivative = np.gradient(inter_pq, 2)
-            q_start_index = find_closest_in_list(len(inter_pq_derivative)/2, find_peaks(inter_pq_derivative))
-            q_start = q_wave - q_start_index
-
-            q_waves.append(q_wave)
-            p_waves.append(p_wave)
-            q_waves_starts.append(q_start)
-        except ValueError:
-            pass
-        except IndexError:
-            pass
-
-        try:
-            epoch_after = np.array(ecg)[int(rpeak):int(rpeaks[index+1])]
-            epoch_after = epoch_after[0:int(len(epoch_after)/2)]
-
-            s_wave_index = np.min(find_peaks(epoch_after))
-            s_wave = rpeak + s_wave_index
-            t_wave_index = s_wave_index + np.argmax(epoch_after[s_wave_index:])
-            t_wave = rpeak + t_wave_index
-
-            inter_st = epoch_after[s_wave_index:t_wave_index]
-            inter_st_derivative = np.gradient(inter_st, 2)
-            t_start_index = find_closest_in_list(len(inter_st_derivative)/2, find_peaks(inter_st_derivative))
-            t_start = s_wave + t_start_index
-            t_end = np.min(find_peaks(epoch_after[t_wave_index:]))
-            t_end = t_wave + t_end
-
-            s_waves.append(s_wave)
-            t_waves.append(t_wave)
-            t_waves_starts.append(t_start)
-            t_waves_ends.append(t_end)
-        except ValueError:
-            pass
-        except IndexError:
-            pass
-
-# pd.Series(epoch_before).plot()
-#    t_waves = []
-#    for index, rpeak in enumerate(rpeaks[0:-1]):
-#
-#        epoch = np.array(ecg)[int(rpeak):int(rpeaks[index+1])]
-#        pd.Series(epoch).plot()
-#
-#        # T wave
-#        middle = (rpeaks[index+1] - rpeak) / 2
-#        quarter = middle/2
-#
-#        epoch = np.array(ecg)[int(rpeak+quarter):int(rpeak+middle)]
-#
-#        try:
-#            t_wave = int(rpeak+quarter) + np.argmax(epoch)
-#            t_waves.append(t_wave)
-#        except ValueError:
-#            pass
-#
-#    p_waves = []
-#    for index, rpeak in enumerate(rpeaks[1:]):
-#        index += 1
-#        # Q wave
-#        middle = (rpeak - rpeaks[index-1]) / 2
-#        quarter = middle/2
-#
-#        epoch = np.array(ecg)[int(rpeak-middle):int(rpeak-quarter)]
-#
-#        try:
-#            p_wave = int(rpeak-quarter) + np.argmax(epoch)
-#            p_waves.append(p_wave)
-#        except ValueError:
-#            pass
-#
-#    q_waves = []
-#    for index, p_wave in enumerate(p_waves):
-#        epoch = np.array(ecg)[int(p_wave):int(rpeaks[rpeaks>p_wave][0])]
-#
-#        try:
-#            q_wave = p_wave + np.argmin(epoch)
-#            q_waves.append(q_wave)
-#        except ValueError:
-#            pass
-#
-#    # TODO: manage to find the begininng of the Q and the end of the T wave so we can extract the QT interval
-
-
-    ecg_waves = {"T_Waves": t_waves,
-                 "P_Waves": p_waves,
-                 "Q_Waves": q_waves,
-                 "S_Waves": s_waves,
-                 "Q_Waves_Onsets": q_waves_starts,
-                 "T_Waves_Onsets": t_waves_starts,
-                 "T_Waves_Ends": t_waves_ends}
-    return(ecg_waves)
 
